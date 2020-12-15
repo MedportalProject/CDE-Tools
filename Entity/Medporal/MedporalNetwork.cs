77970f3951cfc578a4452c08c2f7778d57a30c24
bmiccwangzhe <69947683+bmiccwangzhe@users.noreply.github.com>
2020-12-15 13:18:36 +0800

    Add files via upload

---
+++ Entity/Medporal/MedporalNetwork.cs
@@ -0,0 +1,64 @@
+﻿/// THIS FILE IS PART OF CDE TOOLS PROJECT 
+/// CDE TOOLS, A TERM SEARCH AND ANNOTATION EXCEL-ADDIN.
+///
+/// THIS PROGRAM IS FREE SOFTWARE, IS LICENSED UNDER APACHE LICENSE V2.0
+/// Copyright 2020 Institute of Basic Medical Sciences, Chinese Academy of Medical Sciences
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///    http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+/// 
+/// IN THIS PROGRAM WE USED OTHER OPEN SOURCE PROGRAM AND NCBO BIOPORTAL SERVER REST API:
+/// BIOPORTAL REST-API: http://data.bioontology.org/documentation 
+/// Based partially an example by ncbo in github project ncbo/ncbo_rest_sample_code[1]:
+///    [1]https://github.com/ncbo/ncbo_rest_sample_code
+/// ExcelDNA: zLib license, Copyright (C) 2005-2020 Govert van Drimmelen
+/// Newtonsoft.Json: The MIT License (MIT), Copyright (c) 2007 James Newton-King
+/// npoi: Apache License Version 2.0
+/// SEE FILE LICENSE IN LICENSE FOLDER
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDETools.Entity.Medporal
+{
+    public static class MedporalNetwork
+    {
+        public static string GetJsonResponse(string url)
+        {
+            HttpClient web = new HttpClient();
+            # please replace your own medportal apikey
+            web.DefaultRequestHeaders.Add("Authorization", "apikey token=YOUR OWN MEDPORTAL APIKEY");
+            Task<HttpResponseMessage> asyncResponse = web.GetAsync(url);
+            asyncResponse.Wait();
+            HttpResponseMessage response = asyncResponse.Result;
+
+            Task<string> asyncString = response.Content.ReadAsStringAsync();
+            asyncString.Wait();
+
+            return asyncString.Result;
+        }
+
+        public static T GetResponseAs<T>(string url)
+        {
+            string jsonResponse = GetJsonResponse(url);
+            //MessageBox.Show(JsonConvert.DeserializeObject<T>(jsonResponse).ToString());
+
+            return JsonConvert.DeserializeObject<T>(jsonResponse);
+
+        }
+    }
+}
+
